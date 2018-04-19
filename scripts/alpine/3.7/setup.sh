6363f9ec0fe438cf040dc7ff736c694123a214a0
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-20 04:37:09 +1000

    guard the container env var setup

--- scripts/alpine/3.7/setup.sh
+++ scripts/alpine/3.7/setup.sh
@@ -23,23 +23,24 @@ export DISTRIB_DESCRIPTION="${PRETTY_NAME}"
 if [ -f /bob/common-setup.sh ] 
 then
   source /bob/common-setup.sh
+  
+  echo "export DISTRIB_ID=${ID}" > /etc/profile.d/bob.sh
+  echo "export DISTRIB_RELEASE=${VERSION_ID}" >> /etc/profile.d/bob.sh
+  echo "export DISTRIB_CODENAME=${VERSION_ID%.*}" >> /etc/profile.d/bob.sh
+  echo "export DISTRIB_DESCRIPTION=\"${PRETTY_NAME}\"" >> /etc/profile.d/bob.sh
+  echo "export OCI_USER=${OCI_USER}" >> /etc/profile.d/bob.sh
+  echo "export OCI_USER_ID=${OCI_USER_ID}" >> /etc/profile.d/bob.sh
+  echo "export OCI_USER_GROUP_ID=${OCI_USER_GROUP_ID}" >> /etc/profile.d/bob.sh
+  echo "export OCI_USER_GROUP=${OCI_USER_GROUP}" >> /etc/profile.d/bob.sh
+
+  #
+  # Load required env variables is case we are building 
+  # in the same shell session
+  #
+  for f in /etc/profile.d/*; do source $f; done
+
 else
   source ./bob/${DISTRIB_ID}/${DISTRIB_CODENAME}/common-setup.sh
 fi
-
-echo "export DISTRIB_ID=${ID}" > /etc/profile.d/bob.sh
-echo "export DISTRIB_RELEASE=${VERSION_ID}" >> /etc/profile.d/bob.sh
-echo "export DISTRIB_CODENAME=${VERSION_ID%.*}" >> /etc/profile.d/bob.sh
-echo "export DISTRIB_DESCRIPTION=\"${PRETTY_NAME}\"" >> /etc/profile.d/bob.sh
-echo "export OCI_USER=${OCI_USER}" >> /etc/profile.d/bob.sh
-echo "export OCI_USER_ID=${OCI_USER_ID}" >> /etc/profile.d/bob.sh
-echo "export OCI_USER_GROUP_ID=${OCI_USER_GROUP_ID}" >> /etc/profile.d/bob.sh
-echo "export OCI_USER_GROUP=${OCI_USER_GROUP}" >> /etc/profile.d/bob.sh
-
-#
-# Load required env variables is case we are building 
-# in the same shell session
-#
-for f in /etc/profile.d/*; do source $f; done
 
 # source ./bob/bob.sh
