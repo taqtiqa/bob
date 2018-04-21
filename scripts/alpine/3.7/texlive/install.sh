02e803c3b5fde55f05e645205253431e57a0970b
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-21 19:57:19 +1000

    !check() prior attempts was breaking

--- scripts/alpine/3.7/texlive/install.sh
+++ scripts/alpine/3.7/texlive/install.sh
@@ -7,6 +7,8 @@
 # - user/install.sh ${OCI_USER}
 # - aports/install.sh
 
+for f in /etc/profile.d/*; do source $f; done
+
 alpine_aports_user=${OCI_USER:-bob}
 
 ##
@@ -20,18 +22,18 @@ alpine_aports_user=${OCI_USER:-bob}
 pkg_name=texlive
 pkg_dir=tmp/aports/community/${pkg_name}
 mkdir -p ${pkg_dir}
-src=/bob/aports/artifacts/${pkg_dir}/APKBUILD.${pkg_name}
+src=/bob/aports/artifacts/${pkg_name}/APKBUILD.${pkg_name}
 dst=/${pkg_dir}/APKBUILD
 su - ${alpine_aports_user} -c "cp ${src} ${dst}"
-su - ${alpine_aports_user} -c "cd ${pkg_dir} && abuild checksum"
-su - ${alpine_aports_user} -c "cd ${pkg_dir} && abuild -r"
+su - ${alpine_aports_user} -c "cd /${pkg_dir} && abuild checksum"
+su - ${alpine_aports_user} -c "cd /${pkg_dir} && abuild -r"
 
 ##
 # Copy APKBUILD.texmf-dist into local repo
 pkg_name=texmf-dist
 pkg_dir=tmp/aports/community/${pkg_name}
 mkdir -p ${pkg_dir}
-src=/bob/aports/artifacts/${pkg_dir}/APKBUILD.${pkg_name}
+src=/bob/aports/artifacts/${pkg_name}/APKBUILD.${pkg_name}
 dst=/${pkg_dir}/APKBUILD
 su - ${alpine_aports_user} -c "cp ${src} ${dst}"
 su - ${alpine_aports_user} -c "cd ${pkg_dir} && abuild checksum"
