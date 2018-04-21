a3ccd5a44b69dab2a60aa29314f32d0dc7edc71b
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-22 07:31:52 +1000

    User install changes - breaking?

--- scripts/alpine/3.7/user/install.sh
+++ scripts/alpine/3.7/user/install.sh
@@ -7,9 +7,23 @@ for f in /etc/profile.d/*; do source $f; done
 OCI_USER=${OCI_USER:-bob}
 user_name=${1:-$OCI_USER}
 
-adduser -h /home/${user_name} -G 'wheel' -S -s /bin/ash ${user_name}
-passwd -d ${user_name}
+apk add --no-cache bash shadow
+
+sed --in-place 's/CREATE_MAIL_SPOOL=yes/CREATE_MAIL_SPOOL=no/g' /etc/default/useradd
+
+deluser --remove-home ${user_name}
+useradd --create-home --shell /bin/bash --no-user-group --groups wheel,${OCI_USER_GROUP} \
+        --gid ${OCI_USER_GROUP_ID} --non-unique --uid ${OCI_USER_ID} ${OCI_USER}
+
+chown ${OCI_USER}:${OCI_USER_GROUP} /home/${OCI_USER}
+chmod --recursive --verbose 6755 /home/${OCI_USER}
+chmod --recursive --verbose u=rwx,go=rwx,a+s /home/${OCI_USER}
+
+passwd --delete ${user_name}
 passwd ${user_name}<<EOF
 ${user_name}${user_name}
 ${user_name}${user_name}
 EOF
+
+cp /etc/profile /home/${user_name}/.profile
+chmod +x /home/${user_name}/.profile
