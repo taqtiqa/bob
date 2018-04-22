f64f23ae7614715b180a5839198d372f954e42a7
Mark Van de Vyver <1335713+taqtiqa-admin@users.noreply.github.com>
2018-04-23 06:04:52 +1000

    TeXLive builds OK

--- scripts/alpine/3.7/texlive/install.sh
+++ scripts/alpine/3.7/texlive/install.sh
@@ -49,27 +49,27 @@ su -l ${alpine_aports_user} -c "cd /${pkg_dir} && abuild -r"
 # /home/${alpine_aports_user}/packages/community/x86_64/
 # Size     Name
 # APKINDEX.tar.gz
-# texmf-dist-2017.46770-r0.apk
-# texmf-dist-bibtexextra-2017.46770-r0.apk
-# texmf-dist-fontsextra-2017.46770-r0.apk
-# texmf-dist-formatsextra-2017.46770-r0.apk
-# texmf-dist-full-2017.46770-r0.apk
-# texmf-dist-games-2017.46770-r0.apk
-# texmf-dist-humanities-2017.46770-r0.apk
-# texmf-dist-lang-2017.46770-r0.apk
-# texmf-dist-langchinese-2017.46770-r0.apk
-# texmf-dist-langcyrillic-2017.46770-r0.apk
-# texmf-dist-langextra-2017.46770-r0.apk
-# texmf-dist-langgreek-2017.46770-r0.apk
-# texmf-dist-langjapanese-2017.46770-r0.apk
-# texmf-dist-langkorean-2017.46770-r0.apk
-# texmf-dist-latexextra-2017.46770-r0.apk
-# texmf-dist-most-2017.46770-r0.apk
-# texmf-dist-music-2017.46770-r0.apk
-# texmf-dist-pictures-2017.46770-r0.apk
-# texmf-dist-pstricks-2017.46770-r0.apk
-# texmf-dist-publishers-2017.46770-r0.apk
-# texmf-dist-science-2017.46770-r0.apk
+# ?????? texmf-dist-2017.46770-r0.apk
+#   3.9M texmf-dist-bibtexextra-2017.46770-r0.apk
+# 499.8M texmf-dist-fontsextra-2017.46770-r0.apk
+#   2.2M texmf-dist-formatsextra-2017.46770-r0.apk
+#   1.1K texmf-dist-full-2017.46770-r0.apk
+# 557.6K texmf-dist-games-2017.46770-r0.apk
+# 449.2K texmf-dist-humanities-2017.46770-r0.apk
+#   1.2K texmf-dist-lang-2017.46770-r0.apk
+#  76.3M texmf-dist-langchinese-2017.46770-r0.apk
+#   4.0M texmf-dist-langcyrillic-2017.46770-r0.apk
+#  27.6M texmf-dist-langextra-2017.46770-r0.apk
+#  69.1M texmf-dist-langgreek-2017.46770-r0.apk
+# 128.3M texmf-dist-langjapanese-2017.46770-r0.apk
+#  73.7M texmf-dist-langkorean-2017.46770-r0.apk
+#  14.2M texmf-dist-latexextra-2017.46770-r0.apk
+#   1.2K texmf-dist-most-2017.46770-r0.apk
+#  16.7M texmf-dist-music-2017.46770-r0.apk
+#   4.2M texmf-dist-pictures-2017.46770-r0.apk
+#  27.1M texmf-dist-pstricks-2017.46770-r0.apk
+#  13.5M texmf-dist-publishers-2017.46770-r0.apk
+
 
 ##
 # Copy APKBUILD.texlive into local repo
@@ -82,12 +82,31 @@ su -l ${alpine_aports_user} -c "cp ${src} ${dst}"
 su -l ${alpine_aports_user} -c "cd /${pkg_dir} && abuild checksum"
 su -l ${alpine_aports_user} -c "cd /${pkg_dir} && abuild -r"
 
+# Creates the following in:
+# /home/${alpine_aports_user}/packages/community/x86_64/
+# Size     Name
+# APKINDEX.tar.gz
+#   6.8M texlive-20170524-r6.apk
+# 553.3K texlive-dev-20170524-r6.apk
+# 498.6K texlive-doc-20170524-r6.apk
+# 227.6K texlive-dvi-20170524-r6.apk
+#   1.2K texlive-full-20170524-r6.apk
+#   4.9M texlive-luatex-20170524-r6.apk
+# 273.3K texlive-xetex-20170524-r6.apk
+# 289.3K xdvik-20170524-r6.apk
+
 # Install built package
+cp -f /etc/apk/repositories /etc/apk/repositories.bak
+echo /home/${alpine_aports_user}/packages/testing/ >>/etc/apk/repositories
+echo /home/${alpine_aports_user}/packages/community/ >>/etc/apk/repositories
+echo /home/${alpine_aports_user}/packages/main/ >>/etc/apk/repositories
 pkg_name=texlive-full
 pkg_ver=20170524-r6
 apk add --no-cache --virtual .build-dependencies /home/${alpine_aports_user}/packages/community/x86_64/$pkg_name-$pkg_ver.apk
+cp -f /etc/apk/repositories.bak /etc/apk/repositories
+rm -f /etc/apk/repositories.bak
 
 #
 # Test build
 #
-su -l ${alpine_aports_user} -c "pdflatex -output-format=pdf /bob/texlive/artifacts/texlive-check.tex" 
+su -l ${alpine_aports_user} -c "pdflatex -output-format=pdf -output-directory=/tmp /bob/texlive/artifacts/tmp/texlive-test.tex" 
